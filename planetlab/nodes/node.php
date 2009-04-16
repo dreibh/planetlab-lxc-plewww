4ffa71198c1858c1ada68ba367e9ad3a338302cd
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2009-04-16 10:51:40 +0000

    show sliver peer with link and color

--- planetlab/nodes/node.php
+++ planetlab/nodes/node.php
@@ -248,7 +248,7 @@ $form->start();
 
     foreach ($slices as $slice) {
       $table->row_start();
-      $table->cell ($peers->shortname($peer_id));
+      $peers->cell ($table,$slice['peer_id']);
       $table->cell (l_slice_t ($slice['slice_id'],$slice['name']));
       # xxx l_sliver not implemented yet - what should we show exactly ?
       $table->cell (l_sliver_t ($node_id,$slice['slice_id'],'view'));
