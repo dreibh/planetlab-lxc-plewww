e590b19d86afbce1568cc878d76cc4222604dbdf
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2009-05-05 13:17:41 +0000

    fix for keeping http://validator.w3.org/check happy

--- plekit/php/nifty.php
+++ plekit/php/nifty.php
@@ -7,7 +7,7 @@ require_once 'prototype.php';
 drupal_set_html_head('
 <script type="text/javascript" src="/plekit/niftycorner/niftycube.js"></script>
 <script type="text/javascript" src="/plekit/niftycorner/nifty_init.js"></script>
-<script> Event.observe(window,"load", nifty_init); </script>
+<script type="text/javascript"> Event.observe(window,"load", nifty_init); </script>
 ');
 
 class PlekitNifty {
