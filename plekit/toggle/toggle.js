4ff384f8971fcb26c11a4806c744adbc4bbbb9be
Panos <antoniad@tvwna-ip-d-29.princeton.org>
2010-10-22 10:45:53 -0400

    A generic way to add information messages to toggles

--- plekit/toggle/toggle.js
+++ plekit/toggle/toggle.js
@@ -1,17 +1,17 @@
-/* 
-   $Id$
-*/
 
-function plc_toggle (id) {
-  var area=$('toggle-area-' + id);
-  area.toggle();
-  var visible=$('toggle-image-visible-' + id);
-  var hidden=$('toggle-image-hidden-' + id);
-  if (area.visible()) {
-    visible.show();
-    hidden.hide();
-  } else {
-    visible.hide();
-    hidden.show();
-  }
+function plc_toggle(id){var area=$('toggle-area-'+id);area.toggle();var visible=$('toggle-image-visible-'+id);var hidden=$('toggle-image-hidden-'+id);if(area.visible()){visible.show();hidden.hide();}else{visible.hide();hidden.show();}}
+
+function plc_show_toggle_info(div, id) {
+//debugfilter("showing "+div);
+
+var area=$('toggle-area-'+id);
+var visible=$('toggle-image-visible-'+id);
+var hidden=$('toggle-image-hidden-'+id);
+
+if (document.getElementById(div).style.display == "none") {
+	document.getElementById(div).style.display = "";
+	if (! area.visible())
+		area.toggle();
+}
+
 }
