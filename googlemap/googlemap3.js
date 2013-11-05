63ecd1e73d3792927e452ef8b61078558338dd46
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-11-05 15:18:56 +0100

    a gmap-api v3 compatible replacement for the googlemap

--- googlemap/googlemap3.js
+++ googlemap/googlemap3.js
@@ -1,3 +1,5 @@
+/* the googlemap plugin using api v3 since v2 is bout to be taken down end of 2013 */
+
 /* this function should solve the issue of calling a function
  * upon page load, that badly depends on the browser
  * see an example in http://svn.planet-lab.org/wiki/GooglemapSetup
@@ -22,31 +24,45 @@ function onContent(f){
 /* the PLE snippet for embedding a googlemap in front page */
 /*
 <p><?php
-drupal_set_html_head('<script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=<..>"
-      type="text/javascript"></script>');
-drupal_add_js('googlemap/googlemap.js',TRUE);
+drupal_set_html_head('<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=<...>&sensor=false"></script>');
+drupal_add_js('googlemap/googlemap3.js',TRUE);
 drupal_set_html_head('<script type="text/javascript">
 function my_googlemap () { ts = new Date().getTime(); googlemap("map_area","http://www.planet-lab.eu/sites/sites.kml?" + ts,52,15,4);} 
 onContent(my_googlemap);
 </script>');
-?>
 <div id="map_area" style="width: 640px; height: 480px">  </div></p>
+?>
 */
 
 function googlemap (htmlid,kmlurl,centerLat, centerLon, zoom) {
-  //  alert ('in googlemap, kmlurl='+kmlurl+',id ='+htmlid);
-  if (GBrowserIsCompatible()) {
-    var map = new GMap2(document.getElementById(htmlid));
-    map.setCenter(new GLatLng(centerLat, centerLon), zoom);
-    map.addControl(new GLargeMapControl());
-    map.addControl(new GMapTypeControl());
-    map.addControl(new GOverviewMapControl());
-    /*var geocoder = new GClientGeocoder();*/
-    map.setMapType(G_SATELLITE_MAP);
-    
-    geoXml = new GGeoXml(kmlurl);
-    map.addOverlay(geoXml);
-  }
+    //  alert ('in googlemap, kmlurl='+kmlurl+',id ='+htmlid);
+    /* GBrowserIsCompatible was deprecated in v3
+       if (GBrowserIsCompatible()) ...
+    */
+
+    var center = new google.maps.LatLng(centerLat, centerLon);
+    var options = {
+	zoom: zoom,
+	center: center,
+	mapTypeId: google.maps.MapTypeId.SATELLITEMAP, /* ROADMAP */
+	panControl: true,
+	zoomControl: true,
+	mapTypeControl: true,
+	scaleControl: true,
+	streetViewControl: true,
+	overviewMapControl: true,
+    }
+
+    var map = new google.maps.Map(document.getElementById(htmlid), options);
+
+    var layer_options = {
+	map: map,
+	preserveViewport: true,
+	/*suppressInfoWindows*/
+    };
+    var layer = new google.maps.KmlLayer(kmlurl, layer_options);
 }
 
-window.onunload=GUnload;
+/* GUnload was deprecated in v3
+window.onunload=GUnload; 
+*/
