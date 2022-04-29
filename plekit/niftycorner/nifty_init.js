b3309cf42f3678c1513c63559a4706167faa8a65
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2022-04-29 10:54:33 +0200

    nicer js

--- plekit/niftycorner/nifty_init.js
+++ plekit/niftycorner/nifty_init.js
@@ -1,9 +1,9 @@
-/* 
+/*
   $Id$
 
- instead of explicitly calling 
+ instead of explicitly calling
  Nifty ('div#$id','medium');
- on every single element as the original niftycube.js recommends, 
+ on every single element as the original niftycube.js recommends,
  we just do that on every element that has one of the
  the nifty-{small,medium,big} class set
 */
@@ -11,18 +11,18 @@
 var nifty_inited = false;
 
 function nifty_init () {
+  
   if ( nifty_inited ) return;
-  var sizes= { 0: 'small', 1: 'medium', 2: 'big', length: 3};
-  $A(sizes).each (function (size) {
-    
-    var elements=document.getElementsByClassName('nifty-'+size);
-    for (var i=0; i<elements.length; i++) {
+  const sizes = { 0: 'small', 1: 'medium', 2: 'big', length: 3}
+  for (const [int, size] of Object.entries(sizes)) {
+
+    let elements = document.getElementsByClassName('nifty-'+size)
+    for (const element of elements) {
       // use Rounded rather than Nifty
       // the latter needs an id that some elements don't have
       // plus, it's more efficient anyway
-      pleRounded(elements[i],size);
+      pleRounded(element, size)
     }
-    });
-  nifty_inited = true;
+  }
+  nifty_inited = true
 }
-
